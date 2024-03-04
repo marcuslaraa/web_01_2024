7a23efe28a21ec5846cc8d392346dfdfaa9bc462
marcuslaraa <marcus.lara@hotmail.com>
2024-03-04 15:02:53 -0300

    Finalizando exercicio 03

--- Exercicios/lista_02/dist/js/ex03.js
+++ Exercicios/lista_02/dist/js/ex03.js
@@ -1,15 +1,43 @@
 "use strict";
-function validaCPF(cpf) {
-    verificaPrimeiroDigito(cpf);
-    return false;
+Object.defineProperty(exports, "__esModule", { value: true });
+function conversorStringParaNumber(texto) {
+    let stringArray = texto.split('');
+    return stringArray.map(Number);
 }
 function verificaPrimeiroDigito(cpf) {
     let soma = 0;
     for (let i = 0; i < 9; +i++) {
-        soma += cpf[i] * 10 - i;
-        console.log(soma);
+        soma += cpf[i] * (10 - i);
     }
-    console.log(soma);
+    let resto = (soma * 10) % 11;
+    if (resto == 10) {
+        resto = 0;
+    }
+    if (resto == cpf[9]) {
+        return true;
+    }
     return false;
 }
-validaCPF([5, 2, 9, 9, 8, 2, 2, 4, 7, 2, 5]);
+function verificaSegundoDigito(cpf) {
+    let soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += cpf[i] * (11 - i);
+    }
+    let resto = (soma * 10) % 11;
+    if (resto == cpf[10]) {
+        return true;
+    }
+    return false;
+}
+function validaCPF(cpf) {
+    if (verificaPrimeiroDigito(cpf) && verificaSegundoDigito(cpf)) {
+        return true;
+    }
+    return false;
+}
+if (validaCPF(conversorStringParaNumber('52998224725'))) {
+    console.log("CPF validado com sucesso!!!");
+}
+else {
+    console.log("CPF INVÁLIDO!!!");
+}
--- Exercicios/lista_02/src/ex03.ts
+++ Exercicios/lista_02/src/ex03.ts
@@ -1,20 +1,61 @@
-function validaCPF(cpf:number[]) : boolean {
-    
-    verificaPrimeiroDigito(cpf);
-    
-    return false;
+import { textSpanOverlap } from "typescript";
+
+function conversorStringParaNumber(texto : string) : number[] {
+    let stringArray = texto.split('');
+    return stringArray.map(Number);
 }
+
 
 function verificaPrimeiroDigito(cpf : number[]) : boolean {
     
     let soma:number = 0;
     
     for(let i = 0; i < 9; +i++) {
-        soma += cpf[i] * 10 - i;
-        console.log(soma);
+        soma += cpf[i] * (10 - i);
     }
-    console.log(soma);
+    
+    let resto:number = (soma * 10) % 11;
+
+    if(resto == 10) {
+        resto = 0;
+    }
+
+    if(resto == cpf[9]) {
+        return true;
+    }   
+
     return false;
 }
 
-validaCPF([5,2,9,9,8,2,2,4,7,2,5]);
+function verificaSegundoDigito(cpf:number[]) : boolean {
+
+    let soma : number = 0;
+
+    for(let i = 0; i < 10; i++) {
+        soma += cpf[i] * (11 - i);
+    }
+
+    let resto:number = (soma * 10) % 11;
+    if(resto == cpf[10]) {
+        return true;
+    }
+
+    return false;
+}
+
+function validaCPF(cpf:number[]) : boolean {
+    
+
+    if(verificaPrimeiroDigito(cpf) && verificaSegundoDigito(cpf)) {
+        return true;
+    }
+
+    return false;
+}
+
+
+if(validaCPF(conversorStringParaNumber('52998224725'))) {
+    console.log("CPF validado com sucesso!!!");
+} else {
+    console.log("CPF INVÁLIDO!!!");
+}
